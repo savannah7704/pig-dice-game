4f35274d6a9e8e6b8f03c2bf2c7adb504b029b76
savannah7704 <savannah7704@comcast.net>
2025-02-09 13:07:49 -0600

    Updated pig.js

--- pig.js
+++ pig.js
@@ -1,7 +1,10 @@
+let playerScore = 0
+let playerCurrentScore = 0
+let compScore = 0
+let compCurrentScore = 0
+
 function PlayerTurn(){
-    // manages the player's turn
-    //updates current score
-    //checks for roll of 1
+    RollDie()
 }
 
 function ComputerTurn(){
@@ -11,13 +14,39 @@ function ComputerTurn(){
     //implement strategy for when computer holds. Maybe based on a random number?
 }
 
-function RollDie(){
-// generate a random number 1-6
-// if 1 is rolled, turn ends
+function RollDie(initialRoll = false){
+
+    let roll = Math.floor(Math.random() * 6) + 1
+    if (initialRoll){
+        return roll;
+    } else {
+        if (roll == 1) {
+            playerCurrentScore = 0;
+            alert("You rolled a 1! Your turn is over and you've lost any points gained during this turn.")
+            ComputerTurn()
+        } else {
+            playerCurrentScore += roll;
+            alert("You rolled a " + roll + "! You current score is " + playerCurrentScore + ".")
+        }
+        return roll;
+    }
+    // generate a random number 1-6
+    // if 1 is rolled, turn ends
 }
 
-function Hold(){
+function OnLoadRoll() {
+
+    playerScore = RollDie(true)
+    alert = ("Your initial roll: " + playerScore)
+
+}
+
+function PlayerHold(){
 //Adds the current turn’s points to the player’s total score and switches turns.
+}
+
+function CompHold(){
+
 }
 
 function CheckWinner(){
