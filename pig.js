648e0a1f1005ca786ab932b4d723242d2c8873a5
savannah7704 <savannah7704@comcast.net>
2025-02-09 11:57:41 -0600

    Added files.

---
+++ pig.js
@@ -0,0 +1,29 @@
+function PlayerTurn(){
+    // manages the player's turn
+    //updates current score
+    //checks for roll of 1
+}
+
+function ComputerTurn(){
+    // manages the computer's turn
+    //updates current score
+    //check for roll of 1
+    //implement strategy for when computer holds. Maybe based on a random number?
+}
+
+function RollDie(){
+// generate a random number 1-6
+// if 1 is rolled, turn ends
+}
+
+function Hold(){
+//Adds the current turn’s points to the player’s total score and switches turns.
+}
+
+function CheckWinner(){
+    //Checks if a player has reached 100 points and declares the winner.
+}
+
+function reset(){
+    //reset game
+}
